940f8266d4b2b7082a674274f0ed4338612816f9
PatricioLugo <149405539+PatricioLugo@users.noreply.github.com>
2023-11-29 18:27:34 -0600

    Add files via upload

---
+++ Admin.h
@@ -0,0 +1,26 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+#include <iostream>
+#include <string>
+#include "Usuario.cpp"
+
+using namespace std;
+
+class Admin: public Usuario{
+    //Atributos
+    private:
+        int password;
+    
+    public:
+        //Constructores
+        Admin();
+            Admin(string tipoDeUsuario_, int id_, string nombre_, int password_);
+
+        //Getters
+        int getPassword();
+
+        //Setters
+        void setPassword(int password_);
+};
+
+#endif
